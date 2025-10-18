b99c5673fdda451fe7e953d3a328d6264cbf1675
Bishara1 <bish.150b@gmail.com>
2025-10-18 21:51:53 +0300

    Update functionalities
    • Added input and tile-conquering debouncing to prevent moving more than one tile for a set amount of time for a smoother gameplay.
    • Organized tile conquering and tile highlighting in main loop.
    • Added functionality to interrupt conquering tiles by pressing "q".
    • updated Player::move() so that it returns a boolean if the move was successful or not. Fixed bug where moving to friendly tiles reduces soldiers
    • Tile class: Added __hash__(). And __str__() for debugging
    • main::draw_tiles(): fixed overriding player base sprite with its own tile color.

--- Components.py
+++ Components.py
@@ -16,13 +16,15 @@ class Player(Sprite_Object):
         self.tiles = set()
 
 
-    def move(self, src_tile: "Tile", dest_tile: "Tile"):
+    def move(self, src_tile: "Tile", dest_tile: "Tile") -> bool:
         """Move army from a tile to another tile"""
         # Player should have more than 1 soldier in the source tile to move
         if src_tile.soldiers > 1:
             # Cost of move = 1 soldier
             # 1 soldier stays in the souce tile, the rest move to the destination
-            dest_tile.soldiers = src_tile - 1 - dest_tile.soldiers
+            # friendly_tile helps to know if the destination tile is friendly or not to not kill own soldiers.
+            friendly_tile: bool = dest_tile.player == self if dest_tile.player else False
+            dest_tile.soldiers = src_tile.soldiers - 1 + (friendly_tile * dest_tile.soldiers)
             src_tile.soldiers = 1
 
             if dest_tile.soldiers < 0:
@@ -35,6 +37,11 @@ class Player(Sprite_Object):
                 # Otherwise, this player conquers the tile even if the subtraction
                 # result is 0.
                 dest_tile.player = self
+                # self.tiles.add(dest_tile)
+            
+            return True
+        
+        return False
 
 
 class Mountain(Sprite_Object):
@@ -57,7 +64,13 @@ class Tile():
             return self.pos == other
         
         return NotImplemented
-        
+    
+    def __str__(self):
+        return f"Tile at {self.pos}, with {self.soldiers} soldiers of {self.player}"
+
+    
+    def __hash__(self):
+        return hash(self.pos)
 
 class Outpost(Tile):
     def __init__(self, pos):
--- main.py
+++ main.py
@@ -12,8 +12,10 @@ screen.fill("#DCDCDC")
 font = pygame.font.SysFont('Consolas', 18)
 clock = pygame.time.Clock()
 
-inputs = {pygame.K_UP: (0, -TILE_SIZE), pygame.K_DOWN: (0, TILE_SIZE),
-          pygame.K_LEFT: (-TILE_SIZE, 0), pygame.K_RIGHT: (TILE_SIZE, 0)
+inputs = {pygame.K_UP: (0, -TILE_SIZE), 
+          pygame.K_DOWN: (0, TILE_SIZE),
+          pygame.K_LEFT: (-TILE_SIZE, 0), 
+          pygame.K_RIGHT: (TILE_SIZE, 0)
         }
 
 # Load sprites
@@ -44,7 +46,8 @@ def draw_tiles(positions: iter):
     for position in positions:
         col, row = position
         topleft = (col+TILE_BORDER_SIZE, row+TILE_BORDER_SIZE)
-        pygame.draw.rect(screen, "#2792FF", (*topleft, TILE_BODY_SIZE, TILE_BODY_SIZE))
+        if tiles[position] != player1.base:
+            pygame.draw.rect(screen, "#2792FF", (*topleft, TILE_BODY_SIZE, TILE_BODY_SIZE))
 
 def draw_grid():
     for i in range(N_TILES):
@@ -54,23 +57,22 @@ def draw_grid():
         pygame.draw.line(screen, (10, 10, 10), (0, TILE_SIZE*i), (SCREEN_WIDTH, TILE_SIZE*i), TILE_BORDER_SIZE)
 
 
-def handle_input(event, current_tile_pos):
-    # Choose the desired tile as long as it's not a mountain.
-    direction = inputs[event.key]
-    tile_to_chose_pos = (current_tile_pos[0] + direction[0], current_tile_pos[1] + direction[1])
-    tile_to_chose = tiles.get(tile_to_chose_pos, None)
-
-    return tile_to_chose.pos if tile_to_chose != None else current_tile_pos
+def select_next_tile(current_tile_pos, direction):
+    # Choose the desired tile as long as it's not a mountain or beyond the grid boundaries.
+    tile_to_choose_pos = (current_tile_pos[0] + direction[0], current_tile_pos[1] + direction[1])
+    tile_to_choose = tiles.get(tile_to_choose_pos, None)
+    
+    return tile_to_choose.pos if tile_to_choose else current_tile_pos
 
 
 def highlight_tile(tile_pos: tuple):
     draw_grid()
     highlight_pos = (tile_pos[0]+1, tile_pos[1]+1)
-    pygame.draw.rect(screen, (200, 200, 200), (*highlight_pos, TILE_SIZE, TILE_SIZE), 1)
+    pygame.draw.rect(screen, (220, 220, 220), (*highlight_pos, TILE_SIZE, TILE_SIZE), 2)
+
 
 
 # Players
-player1 = Player(base_image, get_random_location(available_positions), "#2792FF")
 tiles = {}
 
 # keep track of information for available tiles
@@ -80,44 +82,82 @@ for pos in available_positions:
 
 # Draw mountains and grid
 mountains_sprites.draw(screen) 
-screen.blit(player1.image, player1.rect)
 draw_grid()
 draw_tiles(positions)
 
+player1 = Player(base_image, get_random_location(available_positions), "#2792FF")
+tiles[player1.base].player = player1
+tiles[player1.base].soldiers = 2
+player1.tiles.add(tiles[player1.base])
+screen.blit(player1.image, player1.rect)
 
-# army = 1
-# text_surface = font.render(str(army), True, (255, 255, 255))
+
+# text_surface = font.render(str("tst"), True, (255, 255, 255))
 # text_surface_rect = text_surface.get_rect(center=(player1.rect[0]+ TILE_SIZE // 2, player1.rect[1] + TILE_SIZE // 2))
 
 def main():
     curr_chosen_tile_pos = player1.base
+    queue = []
+
+    # Debouncing input and tile-moving and other cooldown variables.
+    last_press_time = 0
+    last_move_time = 0
+    last_regenerate_time = 0
+    last_regenerate_all_time = 0
+    cooldown_select = 150
+    cooldown_move = 500
+    cooldown_regenerate = 1000
+    last_regenerate_all_time = 20000
+    
 
     # game loop
     running = True
     while running:
+        clock.tick(60)
+        current_time = pygame.time.get_ticks()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
-        if event.type == pygame.MOUSEBUTTONUP:
-            x, y = pygame.mouse.get_pos()
-            row = y // TILE_SIZE * TILE_SIZE
-            col = x // TILE_SIZE * TILE_SIZE
-            pos = (col, row)
-            if pos not in mountains_positions and pos != player1.rect.topleft:
-                draw_tiles([pos])
-        
-        if event.type == pygame.KEYDOWN:
-            prev_chosen_tile_pos = curr_chosen_tile_pos
-            curr_chosen_tile_pos = handle_input(event, curr_chosen_tile_pos)
-            if prev_chosen_tile_pos != curr_chosen_tile_pos and curr_chosen_tile_pos != player1.base:
-                draw_tiles([curr_chosen_tile_pos])
-                highlight_tile(curr_chosen_tile_pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = pygame.mouse.get_pos()
+                row = y // TILE_SIZE * TILE_SIZE
+                col = x // TILE_SIZE * TILE_SIZE
+                pos = (col, row)
+                if pos not in mountains_positions and pos != player1.rect.topleft:
+                    draw_tiles([pos])
             
-            else:
-                curr_chosen_tile_pos = prev_chosen_tile_pos
+            if event.type == pygame.KEYDOWN:
+                if current_time - last_press_time > cooldown_select and event.key in inputs.keys():
+                    prev_chosen_tile_pos = curr_chosen_tile_pos
+                    direction = inputs[event.key]
+                    curr_chosen_tile_pos = select_next_tile(curr_chosen_tile_pos, direction)
+                    if prev_chosen_tile_pos != curr_chosen_tile_pos:
+                        highlight_tile(curr_chosen_tile_pos)
+                        queue.append((curr_chosen_tile_pos, prev_chosen_tile_pos))  # add this tile to the queue for tiles to conquer
+                    last_press_time = current_time
+                
+                # Clear queue to stop conquering tiles
+                if event.key == pygame.K_q:
+                    queue.clear()
+
+        if current_time - last_move_time > cooldown_move:
+            if queue:
+                tile_to_conquer_pos, prev_tile_pos = queue.pop(0)
+                if tiles[prev_chosen_tile_pos].player == player1:
+                    if player1.move(tiles[prev_tile_pos], tiles[tile_to_conquer_pos]):
+                        # print("Tile to conquer: " + str(tiles[tile_to_conquer_pos]))
+                        # print("Tile was on: " + str(tiles[player1.base]))
+                        # print()
+                        draw_tiles([tile_to_conquer_pos])
+            
+            last_move_time = current_time
         
-        clock.tick(20)
+        if current_time - last_regenerate_time > cooldown_regenerate:
+            tiles[player1.base].soldiers += 1
+            cooldown_regenerate = current_time
+    
         pygame.display.update()
 
 main()
