c92ab5d1b5a25694e6ba98825914d0d4be7d7691
Bishara1 <bish.150b@gmail.com>
2025-10-21 21:25:54 +0300

    Update functionalities
    • Added input and tile-conquering debouncing to prevent moving more than one tile for a set amount
    • Added text to each tile using Label class.
    • Queue empties itself once both prev and next tiles in the first element of it don't belong to the player.
    • implemented regeneration for all tiles once a set amount of time passed.
    • main::draw_tiles - moved the drawing functionality to Tile class
    • Implemented selecting a tile by mouse.
    • Important note:
        All tiles available for movement are stored in available_tiles dictionary in main. players save references to certain tiles from the dictionary once they are conquered. Updating a tile also updates it in all places where it's referenced.

--- Components.py
+++ Components.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 
+pygame.font.init()
+def_font = pygame.font.SysFont('Consolas', 18)
+
 class Sprite_Object(pygame.sprite.Sprite):
     def __init__(self, image: pygame.image, pos):
         super().__init__()
@@ -8,12 +11,54 @@ class Sprite_Object(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
 
 
+class Label(pygame.sprite.Sprite):
+    def __init__(self, text, position, font=def_font, anchor='center'):
+        super().__init__()
+        self._font = font
+        self._text = text
+        self._color = (255, 255, 255)
+        self._anchor = anchor
+        self._position = position
+        self._render()
+
+    def _render(self):
+        self.image = self._font.render(self._text, 1, self._color)
+        self.rect = self.image.get_rect(**{self._anchor: self._position})
+
+    def clip(self, rect):
+        self.image = self.image.subsurface(rect)
+        self.rect = self.image.get_rect(**{self._anchor: self._position})
+
+    def draw(self, surface: pygame.surface.Surface):
+        surface.blit(self.image, self.rect)
+
+    def set_text(self, text: str):
+        self._text = text
+        self._render()
+
+    def set_font(self, font: pygame.font.Font):
+        self._font = font
+        self._render()
+
+    def set_color(self, color):
+        self._color = color
+        self._render()
+
+    def set_position(self, position, anchor=None):
+        self._position = position
+        if anchor:
+            self._anchor = anchor
+
+        self.rect = self.image.get_rect(**{self._anchor: self._position})
+
+
+
 class Player(Sprite_Object):
     def __init__(self, image: pygame.image, pos, color):
         super().__init__(image, pos)
-        self.base = pos
         self.color = color
-        self.tiles = set()
+        self.base = pos
+        self.tiles: dict[tuple: "Tile"] = {}
 
 
     def move(self, src_tile: "Tile", dest_tile: "Tile") -> bool:
@@ -37,11 +82,20 @@ class Player(Sprite_Object):
                 # Otherwise, this player conquers the tile even if the subtraction
                 # result is 0.
                 dest_tile.player = self
-                # self.tiles.add(dest_tile)
+                self.tiles[dest_tile.pos] = dest_tile
             
             return True
         
         return False
+    
+    def draw_tiles(self, surface):
+        for tile in self.tiles.values():
+            tile.draw(surface)
+    
+
+    def regenerate_all(self):
+        for tile in self.tiles.values():
+            tile.soldiers += 1
 
 
 class Mountain(Sprite_Object):
@@ -50,27 +104,39 @@ class Mountain(Sprite_Object):
 
 
 class Tile():
-    def __init__(self, pos):
+    """A tile in the grid"""
+    def __init__(self, pos, soldiers=0):
         self.pos = pos
-        self.soldiers: int = 0
+        self.soldiers: int = soldiers
         self.player: Player = None
+        label_position = (self.pos[0] + TILE_SIZE // 2, self.pos[1] + TILE_SIZE // 2)
+        self.label = Label("", label_position)
 
-    
     def __eq__(self, other):
         if isinstance(other, Tile):
             return self.pos == other.pos
-
         elif isinstance(other, tuple):
             return self.pos == other
-        
         return NotImplemented
     
     def __str__(self):
         return f"Tile at {self.pos}, with {self.soldiers} soldiers of {self.player}"
-
     
     def __hash__(self):
         return hash(self.pos)
+
+    def draw(self, screen: pygame.surface.Surface):
+        topleft = (self.pos[0] +TILE_BORDER_SIZE, self.pos[1]+TILE_BORDER_SIZE)
+        if self.player:
+            pygame.draw.rect(screen, self.player.color, (*topleft, TILE_BODY_SIZE, TILE_BODY_SIZE))
+            screen.blit(self.player.image, self.player.base)
+            self.label.set_text(str(self.soldiers) if self.soldiers > 0 else "")
+            self.label.draw(screen)
+        
+        else:
+            pygame.draw.rect(screen, "#DCDCDC", (*topleft, TILE_BODY_SIZE, TILE_BODY_SIZE))
+
+
 
 class Outpost(Tile):
     def __init__(self, pos):
--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from constants import *
 from Utils import *
 
 pygame.init()
-pygame.font.init()
 pygame.display.set_caption("Generals")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill("#DCDCDC")
-font = pygame.font.SysFont('Consolas', 18)
 clock = pygame.time.Clock()
 
 inputs = {pygame.K_UP: (0, -TILE_SIZE), 
@@ -41,13 +39,15 @@ available_positions = set([(x//TILE_SIZE * TILE_SIZE, y // TILE_SIZE * TILE_SIZE
                            for y in range(0, SCREEN_HEIGHT, 20)]) - mountains_positions
 
 positions = set()
+available_tiles = {}
+for pos in available_positions:
+    available_tiles[pos] = Tile(pos)
+
 
 def draw_tiles(positions: iter):
     for position in positions:
-        col, row = position
-        topleft = (col+TILE_BORDER_SIZE, row+TILE_BORDER_SIZE)
-        if tiles[position] != player1.base:
-            pygame.draw.rect(screen, "#2792FF", (*topleft, TILE_BODY_SIZE, TILE_BODY_SIZE))
+        available_tiles[position].draw(screen)
+    
 
 def draw_grid():
     for i in range(N_TILES):
@@ -59,10 +59,10 @@ def draw_grid():
 
 def select_next_tile(current_tile_pos, direction):
     # Choose the desired tile as long as it's not a mountain or beyond the grid boundaries.
-    tile_to_choose_pos = (current_tile_pos[0] + direction[0], current_tile_pos[1] + direction[1])
-    tile_to_choose = tiles.get(tile_to_choose_pos, None)
-    
-    return tile_to_choose.pos if tile_to_choose else current_tile_pos
+    tile_to_choose_pos = current_tile_pos
+    if direction != (0, 0):
+        tile_to_choose_pos = (current_tile_pos[0] + direction[0], current_tile_pos[1] + direction[1])
+    return tile_to_choose_pos if tile_to_choose_pos in available_positions else current_tile_pos
 
 
 def highlight_tile(tile_pos: tuple):
@@ -71,24 +71,15 @@ def highlight_tile(tile_pos: tuple):
     pygame.draw.rect(screen, (220, 220, 220), (*highlight_pos, TILE_SIZE, TILE_SIZE), 2)
 
 
-
-# Players
-tiles = {}
-
-# keep track of information for available tiles
-for pos in available_positions:
-    tiles[pos] = Tile(pos)
-
+player1 = Player(base_image, get_random_location(available_positions), "#2792FF")
 
 # Draw mountains and grid
 mountains_sprites.draw(screen) 
+player1.draw_tiles(screen)
 draw_grid()
-draw_tiles(positions)
-
-player1 = Player(base_image, get_random_location(available_positions), "#2792FF")
-tiles[player1.base].player = player1
-tiles[player1.base].soldiers = 2
-player1.tiles.add(tiles[player1.base])
+available_tiles[player1.base].player = player1
+available_tiles[player1.base].soldiers = 1
+player1.tiles[player1.base] = available_tiles[player1.base]
 screen.blit(player1.image, player1.rect)
 
 
@@ -104,10 +95,10 @@ def main():
     last_move_time = 0
     last_regenerate_time = 0
     last_regenerate_all_time = 0
-    cooldown_select = 150
+    cooldown_select = 50
     cooldown_move = 500
     cooldown_regenerate = 1000
-    last_regenerate_all_time = 20000
+    cooldown_regenerate_all = 20000
     
 
     # game loop
@@ -124,10 +115,11 @@ def main():
                 x, y = pygame.mouse.get_pos()
                 row = y // TILE_SIZE * TILE_SIZE
                 col = x // TILE_SIZE * TILE_SIZE
-                pos = (col, row)
-                if pos not in mountains_positions and pos != player1.rect.topleft:
-                    draw_tiles([pos])
-            
+                if (col, row) in available_tiles:
+                    tile_to_highlight = select_next_tile((col, row), (0, 0))
+                    highlight_tile(tile_to_highlight)
+                    prev_chosen_tile_pos = curr_chosen_tile_pos = tile_to_highlight
+
             if event.type == pygame.KEYDOWN:
                 if current_time - last_press_time > cooldown_select and event.key in inputs.keys():
                     prev_chosen_tile_pos = curr_chosen_tile_pos
@@ -142,22 +134,32 @@ def main():
                 if event.key == pygame.K_q:
                     queue.clear()
 
-        if current_time - last_move_time > cooldown_move:
-            if queue:
-                tile_to_conquer_pos, prev_tile_pos = queue.pop(0)
-                if tiles[prev_chosen_tile_pos].player == player1:
-                    if player1.move(tiles[prev_tile_pos], tiles[tile_to_conquer_pos]):
-                        # print("Tile to conquer: " + str(tiles[tile_to_conquer_pos]))
-                        # print("Tile was on: " + str(tiles[player1.base]))
-                        # print()
-                        draw_tiles([tile_to_conquer_pos])
+        if queue and current_time - last_move_time > cooldown_move:
+            tile_to_conquer_pos, prev_tile_pos = queue.pop(0)
+            if tile_to_conquer_pos not in player1.tiles and prev_tile_pos not in player1.tiles:
+                queue.clear()
+
+            elif player1.tiles.get(prev_tile_pos, None):
+                if player1.move(available_tiles[prev_tile_pos], available_tiles[tile_to_conquer_pos]):
+                    draw_tiles([tile_to_conquer_pos, prev_tile_pos])
             
             last_move_time = current_time
         
+        
         if current_time - last_regenerate_time > cooldown_regenerate:
-            tiles[player1.base].soldiers += 1
-            cooldown_regenerate = current_time
+            available_tiles[player1.base].soldiers += 1
+            last_regenerate_time = current_time
+        
+        if current_time - last_regenerate_all_time > cooldown_regenerate_all:
+            player1.regenerate_all()
+            player1.draw_tiles(screen)
+
+            last_regenerate_all_time = current_time
     
+        draw_tiles([player1.base])
+        draw_grid()
+        highlight_tile(curr_chosen_tile_pos)
+
         pygame.display.update()
 
 main()
